e3a25465c224549398c66ddb716a04c9c69fa15f
SAJIN KASIM <SAJIN KASIM>
2017-01-04 23:33:59 +0530

    move pizzadiv outside loop

--- views/js/main.js
+++ views/js/main.js
@@ -476,8 +476,9 @@ var resizePizzas = function(size) {
 window.performance.mark("mark_start_generating"); // collect timing data
 
 // This for-loop actually creates and appends all of the pizzas when the page loads
+//move pizzaDiv outside loop to avoid unnecssary calls
+var pizzasDiv = document.getElementById("randomPizzas");
 for (var i = 2; i < 100; i++) {
-    var pizzasDiv = document.getElementById("randomPizzas");
     pizzasDiv.appendChild(pizzaElementGenerator(i));
 }
 
