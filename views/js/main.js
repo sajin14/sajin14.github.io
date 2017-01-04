aed0d74da51b5197ac7e550a9044d11de341ab00
SAJIN KASIM <SAJIN KASIM>
2017-01-04 23:02:16 +0530

    testing

--- views/js/main.js
+++ views/js/main.js
@@ -541,7 +541,7 @@ document.addEventListener('DOMContentLoaded', function() {
         var elem = document.createElement('img');
         elem.className = 'mover';
         elem.src = "images/pizza.png";
-        // elem.style.height = "100px";
+        elem.style.height = "100px";
         elem.style.width = "73.333px";
         elem.basicLeft = (i % cols) * s;
         elem.style.top = (Math.floor(i / cols) * s) + 'px';
